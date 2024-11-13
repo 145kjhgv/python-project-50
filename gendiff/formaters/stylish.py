005389cf6ec7a5e5c6c79d735f9b1af621975932
artem <artem_smirnov88@mail.ru>
2024-11-13 18:03:47 +0300

    fix linter

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -28,25 +28,30 @@ def line_forming(dictionary: dict, key: Any, depth: int, sign: str) -> str:
 
 def build_stylish_iter(diff: dict, depth=0) -> str:
     lines = ['{']
+
+    operation_map = {
+        'same': ('value', '    '),
+        'add': ('new', '  + '),
+        'removed': ('old', '  - '),
+        'changed': ('old', '  - '),
+        'nested': None
+    }
+
     for dictionary in diff:
         operation = dictionary['operation']
 
-        if operation == 'same':
-            lines.append(line_forming(dictionary, 'value', depth, sign='    '))
-
-        elif operation == 'add':
-            lines.append(line_forming(dictionary, 'new', depth, sign='  + '))
-
-        elif operation in ('removed', 'changed'):
-            lines.append(line_forming(dictionary, 'old', depth, sign='  - '))
-
-        if operation == 'changed':
-            lines.append(line_forming(dictionary, 'new', depth, sign='  + '))
-
-        elif operation == 'nested':
+        if operation == 'nested':
             new_value = build_stylish_iter(dictionary['value'],
                                            depth + DEFAULT_INDENT)
             lines.append(f'{" " * depth}    {dictionary["key"]}: {new_value}')
+        else:
+            if operation in operation_map:
+                key, sign = operation_map[operation]
+                lines.append(line_forming(dictionary, key, depth, sign=sign))
+
+            if operation == 'changed':
+                lines.append(line_forming(dictionary, 'new',
+                                          depth, sign='  + '))
 
     lines.append(f'{" " * depth}}}')
     return '\n'.join(lines)
